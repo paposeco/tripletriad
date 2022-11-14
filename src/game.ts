a9738a71db6d3cabf55927c984fab152566b83cd
paposeco <13892562+paposeco@users.noreply.github.com>
2022-11-14 23:44:03 +0100

    simplified version of game is working

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,16 @@
+import { captureCard, updateScore } from "./index";
+
 interface Card {
     name: string;
     power: [number, number, number, number];
     face: string;
     cardID: string;
+}
+
+interface Square {
+    playerID: string;
+    cardID: string;
+    power: [number, number, number, number];
 }
 
 const cardcollection = new Map<string, Card>();
@@ -14,9 +22,11 @@ const populateCardCollection = function() {
     cardcollection.set("cardid4", { name: "coeurl", power: [2, 5, 2, 5], face: "", cardID: "cardid4" });
 }
 
+
 populateCardCollection();
 
 
+const board = new Map<string, Square>();
 
 export default function playerDeck(cards: string[]): Card[] {
     let decklist: Card[] = [];
@@ -43,24 +53,120 @@ export function getcardfromcollection(cardid: string): Card | undefined {
 
 //export { cardcollection };
 
+const checkAdjSquares = function(squareid: string): string[] {
+    let squaresToCheck: string[] = [];
+    switch (squareid) {
+        case "topleft":
+            squaresToCheck.push(null, "topmid", "midleft", null);
+            break;
+        case "topmid":
+            squaresToCheck.push(null, "topright", "midmid", "topleft");
+            break;
+        case "topright":
+            squaresToCheck.push(null, null, "midright", "topmid");
+            break;
+        case "midleft":
+            squaresToCheck.push("topleft", "midmid", "bottomleft", null);
+            break;
+        case "midmid":
+            squaresToCheck.push("topmid", "midright", "bottommid", "midleft");
+            break;
+        case "midright":
+            squaresToCheck.push("topright", null, "bottomright", "midmid");
+            break;
+        case "bottomleft":
+            squaresToCheck.push("midleft", "bottommid", null, null);
+            break;
+        case "bottommid":
+            squaresToCheck.push("midmid", "bottomright", null, "bottomleft");
+            break;
+        case "bottomright":
+            squaresToCheck.push("midright", null, null, "bottommid");
+            break;
+        default:
+            console.log("error");
+    }
+    return squaresToCheck;
+}
+
+const powerToCheckCoord = function(currentsquarecoord: number): number {
+    switch (currentsquarecoord) {
+        case 0:
+            return 2;
+        case 1:
+            return 3;
+        case 2:
+            return 0;
+        case 3:
+            return 1;
+        default:
+            return -1;
+    }
+}
+
+const checkCardsPower = function(squarestocheck: string[], playerid: string, cardid: string) {
+    const cardpowers = cardcollection.get(cardid).power;
+    if (cardpowers !== undefined) {
+        for (let i = 0; i < squarestocheck.length; i++) {
+            if (board.has(squarestocheck[i])) {
+                // falta ver de quem é a carta
+                const square = board.get(squarestocheck[i]);
+                if (square.playerID === playerid) {
+                    continue;
+                } else {
+                    const powerCardOnSquare = square.power;
+                    const coordForPower = powerToCheckCoord(i);
+                    if (coordForPower !== -1) {
+                        const powerOnAdjSquare = powerCardOnSquare[coordForPower];
+                        const powerOfNewCard = cardpowers[i];
+                        if (powerOnAdjSquare < powerOfNewCard) {
+                            board.set(squarestocheck[i], { playerID: playerid, cardID: square.cardID, power: square.power });
+                            captureCard(squarestocheck[i], playerid);
+                            updateScore(playerid);
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
 export const movesperround = () => {
     let moves: string[] = [];
-    const redplayercard = function(move: string) {
-        moves.push(move);
+    // cards captures by each player, total cards per each player, map of cards on each location, with powers 
+    const redplayercard = function(cardmove: string) {
+        moves.push(cardmove);
     }
-    const redplayersquare = function(move: string) {
-        moves.push(move);
+    const redplayersquare = function(squaremove: string) {
+        const currentcardid: string = moves[moves.length - 1];
+        const cardStats = cardcollection.get(currentcardid);
+        if (cardStats !== undefined) {
+            board.set(squaremove, { playerID: "redplayer", cardID: currentcardid, power: [cardStats.power[0], cardStats.power[1], cardStats.power[2], cardStats.power[3]] });
+        }
+        moves.push(squaremove);
+        //once card is placed, need to see what to adjacent cards
+        const squaresToCheck = checkAdjSquares(squaremove);
+        checkCardsPower(squaresToCheck, "redplayer", currentcardid);
     }
-    const blueplayercard = function(move: string) {
-        moves.push(move);
+    const blueplayercard = function(cardmove: string) {
+        moves.push(cardmove);
     }
-    const blueplayersquare = function(move: string) {
-        moves.push(move);
+    const blueplayersquare = function(squaremove: string) {
+        const currentcardid: string = moves[moves.length - 1];
+        const cardStats = cardcollection.get(currentcardid);
+        if (cardStats !== undefined) {
+            board.set(squaremove, { playerID: "blueplayer", cardID: currentcardid, power: [cardStats.power[0], cardStats.power[1], cardStats.power[2], cardStats.power[3]] });
+        }
+        moves.push(squaremove);
+        const squaresToCheck = checkAdjSquares(squaremove);
+        checkCardsPower(squaresToCheck, "blueplayer", currentcardid);
     }
-    const gameended = function() {
-        console.log(moves);
-    }
-    return { redplayercard, redplayersquare, blueplayercard, blueplayersquare, gameended }
+    return { redplayercard, redplayersquare, blueplayercard, blueplayersquare }
 };
 
 //export {redplayermove.roundmoves }
@@ -71,5 +177,3 @@ export function updateDeck(playerid: string, cardid: string) {
 }
 
 export { blueplayerdeck, redplayerdeck }
-
-// need a function that given a player id, returns its current deck
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { blueplayerdeck, redplayerdeck, movesperround } from "./game";
 import getcardfromcollection from "./game";
 
 interface Card {
-  name: string;
-  power: [number, number, number, number];
-  face: string;
-  cardID: string;
+    name: string;
+    power: [number, number, number, number];
+    face: string;
+    cardID: string;
 }
 
 
@@ -25,145 +25,189 @@ interface Card {
 const keeptrackofmoves = movesperround();
 
 const round = async function(redplayerdeck: Card[], blueplayerdeck: Card[], emptysquares: number) {
-  let selectedsquare: string = "";
-  let cardselected: string = "";
-  let currentemptysquares = emptysquares;
-  let playerblueplayed = false;
-  let redplayerplayed = false;
+    let selectedsquare: string = "";
+    let cardselected: string = "";
+    let currentemptysquares = emptysquares;
+    let playerblueplayed = false;
+    let redplayerplayed = false;
 
-  const selectCard = function(this: HTMLElement) {
-    this.classList.add("selected");
-    const currentplayer = this.parentElement.id;
+    const selectCard = function(this: HTMLElement) {
+        this.classList.add("selected");
+        const currentplayer = this.parentElement.id;
 
-    if (currentplayer !== null) {
-      const playerdiv = document.getElementById(currentplayer);
-      if (playerdiv !== null) {
-        const playerdeck = playerdiv.children;
-        if (playerdeck.length !== 0) {
-          for (let i = 0; i < playerdeck.length; i++) {
-            const child = playerdeck[i];
-            child.removeEventListener("click", selectCard);
-          }
-          //round(this.dataset.card);
-          cardselected = this.dataset.card;
-          if (currentplayer === "redplayer") {
-            keeptrackofmoves.redplayercard(cardselected);
-          } else if (currentplayer === "blueplayer") {
-            keeptrackofmoves.blueplayercard(cardselected);
-          }
-          if (cardselected !== "" && cardselected !== undefined) {
-            createListenerOnBoard();
-          }
+        if (currentplayer !== null) {
+            const playerdiv = document.getElementById(currentplayer);
+            if (playerdiv !== null) {
+                const playerdeck = playerdiv.children;
+                if (playerdeck.length !== 0) {
+                    for (let i = 0; i < playerdeck.length; i++) {
+                        const child = playerdeck[i];
+                        child.removeEventListener("click", selectCard);
+                    }
+                    //round(this.dataset.card);
+                    cardselected = this.dataset.card;
+                    if (currentplayer === "redplayer") {
+                        keeptrackofmoves.redplayercard(cardselected);
+                    } else if (currentplayer === "blueplayer") {
+                        keeptrackofmoves.blueplayercard(cardselected);
+                    }
+                    if (cardselected !== "" && cardselected !== undefined) {
+                        createListenerOnBoard();
+                    }
+                }
+            }
         }
-      }
     }
-  }
 
-  const deckListener = function(playerid: string) {
-    const playerdiv = document.getElementById(playerid);
-    const subdivs = playerdiv.children;
-    if (subdivs.length !== 0) {
-      for (let i = 0; i < subdivs.length; i++) {
-        subdivs[i].addEventListener("click", selectCard);
-      }
-    }
-  }
-
-  // display both decks, add listener later
-  const displayDeck = function(playerid: string, deck: Card[]): void {
-    const numberofcards = deck.length;
-    const playerdiv = document.getElementById(playerid);
-    if (numberofcards > 0 && playerdiv.children.length === 0) {
-      if (playerdiv !== null) {
-        for (let i = 0; i < numberofcards; i++) {
-          const newdiv = document.createElement("div");
-          playerdiv.appendChild(newdiv);
-          const title = document.createElement("h3");
-          title.classList.add("cardtitle");
-          title.textContent = deck[i].name;
-          newdiv.appendChild(title);
-          const cardpower = document.createElement("p");
-          cardpower.textContent = `${deck[i].power[0]}, ${deck[i].power[1]}, ${deck[i].power[2]}, ${deck[i].power[3]}`;
-          newdiv.appendChild(cardpower);
-          newdiv.dataset.card = `${deck[i].cardID}`;
+    const deckListener = function(playerid: string) {
+        const playerdiv = document.getElementById(playerid);
+        const subdivs = playerdiv.children;
+        if (subdivs.length !== 0) {
+            for (let i = 0; i < subdivs.length; i++) {
+                subdivs[i].addEventListener("click", selectCard);
+            }
         }
-      }
-    }
-  }
-
-  const selectSquare = function(this: HTMLElement) {
-    // place card
-    selectedsquare = this.id;
-
-    const card = getcardfromcollection([cardselected]);
-    if (card !== undefined) {
-      const cardtitle = document.createElement("h3");
-      this.appendChild(cardtitle);
-      cardtitle.textContent = card[0].name;
-      const newdiv = document.createElement("div");
-      this.appendChild(newdiv);
-      newdiv.classList.add("boardcard");
-      const toppower = document.createElement("div");
-      newdiv.appendChild(toppower);
-      toppower.textContent = `${card[0].power[0]}`;
-      const midpowers = document.createElement("div");
-      newdiv.appendChild(midpowers);
-      midpowers.classList.add("boardcardmid");
-      const rightpower = document.createElement("div");
-      midpowers.appendChild(rightpower);
-      rightpower.textContent = `${card[0].power[1]}`;
-      const leftpower = document.createElement("div");
-      midpowers.appendChild(leftpower);
-      leftpower.textContent = `${card[0].power[2]}`;
-      const bottompower = document.createElement("div");
-      newdiv.appendChild(bottompower);
-      bottompower.textContent = `${card[0].power[3]}`;
-
-    }
-    // remove listeners
-    const emptyboardsquares = document.querySelectorAll(".emptysquare");
-    emptyboardsquares.forEach((square) => {
-      square.removeEventListener("click", selectSquare);
-    })
-    this.classList.remove("emptysquare");
-
-    if (!redplayerplayed) {
-      redplayerplayed = true;
-      this.classList.add("redplayercard");
-      keeptrackofmoves.redplayersquare(selectedsquare);
-      --currentemptysquares;
-      if (currentemptysquares === 0) {
-        keeptrackofmoves.gameended();
-        return;
-      }
-      deckListener("blueplayer");
-    } else {
-      keeptrackofmoves.blueplayersquare(selectedsquare);
-      --currentemptysquares;
-      playerblueplayed = true;
-      this.classList.add("blueplayercard");
     }
 
-    if (currentemptysquares > 0 && redplayerplayed && playerblueplayed) {
-      round(redplayerdeck, blueplayerdeck, currentemptysquares);
-    } else if (currentemptysquares === 0) {
-      return;
+    // display both decks, add listener later
+    const displayDeck = function(playerid: string, deck: Card[]): void {
+        const numberofcards = deck.length;
+        const playerdiv = document.getElementById(playerid);
+        if (numberofcards > 0 && playerdiv.children.length === 0) {
+            if (playerdiv !== null) {
+                const h2player = document.createElement("h2");
+                playerdiv.appendChild(h2player);
+                if (playerid === "redplayer") {
+                    h2player.textContent = "Red Player";
+                } else {
+                    h2player.textContent = "Blue Player";
+                }
+                for (let i = 0; i < numberofcards; i++) {
+                    const newdiv = document.createElement("div");
+                    playerdiv.appendChild(newdiv);
+                    const title = document.createElement("h3");
+                    title.classList.add("cardtitle");
+                    title.textContent = deck[i].name;
+                    newdiv.appendChild(title);
+                    newdiv.classList.add("boardcard");
+
+
+                    // const cardpower = document.createElement("p");
+                    // cardpower.textContent = `${deck[i].power[0]}, ${deck[i].power[1]}, ${deck[i].power[2]}, ${deck[i].power[3]}`;
+                    // newdiv.appendChild(cardpower);
+
+                    const toppower = document.createElement("div");
+                    toppower.textContent = `${deck[i].power[0]}`;
+                    const midpowers = document.createElement("div");
+                    const rightpower = document.createElement("div");
+                    rightpower.textContent = `${deck[i].power[1]}`;
+                    const bottompower = document.createElement("div");
+                    bottompower.textContent = `${deck[i].power[2]}`;
+                    const leftpower = document.createElement("div");
+                    leftpower.textContent = `${deck[i].power[3]}`;
+                    midpowers.appendChild(leftpower);
+                    midpowers.appendChild(rightpower);
+                    newdiv.appendChild(toppower);
+                    newdiv.appendChild(midpowers);
+                    newdiv.appendChild(bottompower);
+                    newdiv.dataset.card = `${deck[i].cardID}`;
+                    midpowers.classList.add("boardcardmid");
+                }
+            }
+        }
     }
-  }
+
+    const selectSquare = function(this: HTMLElement) {
+        // place card
+        selectedsquare = this.id;
+
+        const card = getcardfromcollection([cardselected]);
+        if (card !== undefined) {
+            const cardtitle = document.createElement("h3");
+            this.appendChild(cardtitle);
+            cardtitle.textContent = card[0].name;
+            const newdiv = document.createElement("div");
+            this.appendChild(newdiv);
+            newdiv.classList.add("boardcard");
+            const toppower = document.createElement("div");
+            newdiv.appendChild(toppower);
+            toppower.textContent = `${card[0].power[0]}`;
+            const midpowers = document.createElement("div");
+            newdiv.appendChild(midpowers);
+            midpowers.classList.add("boardcardmid");
+            const rightpower = document.createElement("div");
+            rightpower.textContent = `${card[0].power[1]}`;
+            const leftpower = document.createElement("div");
+            midpowers.appendChild(leftpower);
+            midpowers.appendChild(rightpower);
+            leftpower.textContent = `${card[0].power[3]}`;
+            const bottompower = document.createElement("div");
+            newdiv.appendChild(bottompower);
+            bottompower.textContent = `${card[0].power[2]}`;
+
+        }
+        // remove listeners
+        const emptyboardsquares = document.querySelectorAll(".emptysquare");
+        emptyboardsquares.forEach((square) => {
+            square.removeEventListener("click", selectSquare);
+        })
+        this.classList.remove("emptysquare");
+
+        if (!redplayerplayed) {
+            redplayerplayed = true;
+            this.classList.add("redplayercard");
+            keeptrackofmoves.redplayersquare(selectedsquare);
+            --currentemptysquares;
+            if (currentemptysquares === 0) {
+                const currentscoreRed = Number(document.getElementById("scorered").textContent);
+                const currentscoreBlue = Number(document.getElementById("scoreblue").textContent);
+                if (currentscoreRed === currentscoreBlue) {
+                    alert("it's a draw");
+                } else if (currentscoreRed > currentscoreBlue) {
+                    alert("red player wins");
+                } else {
+                    alert("blue player wins");
+                }
+                return;
+            }
+            deckListener("blueplayer");
+        } else {
+            keeptrackofmoves.blueplayersquare(selectedsquare);
+            --currentemptysquares;
+            playerblueplayed = true;
+            this.classList.add("blueplayercard");
+        }
+
+        if (currentemptysquares > 0 && redplayerplayed && playerblueplayed) {
+            round(redplayerdeck, blueplayerdeck, currentemptysquares);
+        } else if (currentemptysquares === 0) {
+            const currentscoreRed = Number(document.getElementById("scorered").textContent);
+            const currentscoreBlue = Number(document.getElementById("scoreblue").textContent);
+            if (currentscoreRed === currentscoreBlue) {
+                alert("it's a draw");
+            } else if (currentscoreRed > currentscoreBlue) {
+                alert("red player wins");
+            } else {
+                alert("blue player wins");
+            }
+
+            return;
+        }
+    }
 
 
 
-  const createListenerOnBoard = function() {
-    const emptyboardsquares = document.querySelectorAll(".emptysquare");
-    emptyboardsquares.forEach((square) => {
-      square.addEventListener("click", selectSquare);
-    })
-  }
-  //first
-  displayDeck("redplayer", redplayerdeck);
-  deckListener("redplayer");
-  //second
-  displayDeck("blueplayer", blueplayerdeck);
+    const createListenerOnBoard = function() {
+        const emptyboardsquares = document.querySelectorAll(".emptysquare");
+        emptyboardsquares.forEach((square) => {
+            square.addEventListener("click", selectSquare);
+        })
+    }
+    //first
+    displayDeck("redplayer", redplayerdeck);
+    deckListener("redplayer");
+    //second
+    displayDeck("blueplayer", blueplayerdeck);
 
 }
 
@@ -190,3 +234,33 @@ round(redplayerdeck, blueplayerdeck, 9);
 
 
 // game script should keep track of deck and current card; 
+export const captureCard = function(squareid: string, playerid: string) {
+    const div = document.getElementById(squareid);
+    if (div !== undefined) {
+        if (playerid === "redplayer") {
+            div.classList.replace("blueplayercard", "redplayercard");
+        } else if (playerid === "blueplayer") {
+            div.classList.replace("redplayercard", "blueplayercard");
+        }
+    }
+}
+
+export const updateScore = function(playerid: string) {
+    if (playerid === "redplayer") {
+        const scorediv = document.getElementById("scorered");
+        const currentscore = Number(scorediv.textContent);
+        scorediv.textContent = `${currentscore + 1}`;
+        const scoredivblue = document.getElementById("scoreblue");
+        const currentscoreblue = Number(scoredivblue.textContent);
+        scoredivblue.textContent = `${currentscoreblue - 1}`;
+    } else if (playerid === "blueplayer") {
+        const scorediv = document.getElementById("scorered");
+        const currentscore = Number(scorediv.textContent);
+        scorediv.textContent = `${currentscore - 1}`;
+        const scoredivblue = document.getElementById("scoreblue");
+        const currentscoreblue = Number(scoredivblue.textContent);
+        scoredivblue.textContent = `${currentscoreblue + 1}`;
+    }
+}
+
+// fim do jogo
